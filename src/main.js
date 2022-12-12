30eda0b2f292727c1a7b7973bed6041b4d649526
Krzysztof <ki.zwolak@gmail.com>
2022-12-13 00:35:49 +0100

    Add main display on hover logic

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+const menus = document.querySelectorAll('.menu');
+
+menus.forEach((menu) => {
+  menu.addEventListener('mouseover', () => {
+    const child = menu.querySelector('.submenu');
+    child.style.display = 'block';
+  });
+  menu.addEventListener('mouseout', () => {
+    const child = menu.querySelector('.submenu');
+    child.style.display = 'none';
+  });
+});
